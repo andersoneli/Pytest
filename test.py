7a7a8c04bc01614efaa7202cbd7c9e4041eea5fe
Eli Anderson <eliaanderson@uiowa.edu>
2023-04-13 12:27:26 -0500

    New Code

---
+++ test.py
@@ -0,0 +1,52 @@
+import pytest
+from oop_loan_pmt import Loan
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+# Unit tests for Loan class
+def test_discount_factor():
+    """
+    GIVEN the user enters their loan details
+    WHEN the loan object's calculateDiscountFactor method is called
+    THEN the discount factor is accurately calculated
+    """
+    loan = Loan(100000, 30, 0.06)
+    loan.calculateDiscountFactor()
+    print("\r")
+    print(" -- calculateDiscountFactor method unit test")
+    assert round(loan.getDiscountFactor(), 4) == 166.7916
+
+def test_loan_payment():
+    """
+    GIVEN the user enters their loan details
+    WHEN the loan object's calculateLoanPmt method is called
+    THEN the loan payment is accurately calculated
+    """
+    loan = Loan(100000, 30, 0.06)
+    loan.calculateLoanPmt()
+    print("\r")
+    print(" -- calculateLoanPmt method unit test")
+    assert round(loan.getLoanPmt(), 2) == 599.55
+
+# Functional tests
+def test_collect_loan_details(monkeypatch):
+    inputs = ["100000", "30", "0.06"]
+    monkeypatch.setattr('builtins.input', lambda _: inputs.pop(0))
+    loan = collectLoanDetails()
+    assert loan.loanAmount == 100000
+    assert loan.numberOfPmts == 360
+    assert loan.annualRate == 0.06
+
+# Integration test
+def test_main_output(capsys, monkeypatch):
+    inputs = ["100000", "30", "0.06"]
+    monkeypatch.setattr('builtins.input', lambda _: inputs.pop(0))
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "Your monthly payment is: $599.55\n"
+
+
+
